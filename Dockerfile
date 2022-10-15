fc9b9cae57ec3f516b60015eed76e91ae274ee3b
The Noah <thenoahbz@gmail.com>
2022-10-15 18:24:04 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM docker:latest
+
+RUN apk add python3 --no-cache --no-scripts && \
+  python3 -m ensurepip --upgrade && \
+  pip3 install s3cmd --upgrade && \
+  rm -rf /var/cache/apk
+
+ADD run.sh /run.sh
+RUN chmod +x /run.sh
+
+CMD ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Missing volume name"
+  exit 1
+fi
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting backup of volume $1..."
+
+filename="$1_$(date +"%Y%m%d").tar.gz"
+
+docker run --rm \
+  -v $1:ro:/backup-volume \
+  busybox \
+  tar -zcvf /tmp/$filename -C /backup-volume .
+
+s3cmd put --host=$S3_HOST --host-bucket=$S3_HOST_BUCKET /tmp/$filename s3://$S3_BUCKET/$(date +"%Y")/$(date +"%m")/$filename || exit 2
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Done!"
