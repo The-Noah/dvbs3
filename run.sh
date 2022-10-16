71c70a98afbc640296e51ad682e4672798944904
The Noah <thenoahbz@gmail.com>
2022-10-15 20:20:17 -0500

    fix: resulting tarball entries starting with "./"

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ fi
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting backup of volume $1..."
 
-filename="$1_$(date +"%Y%m%d").tar.gz"
+filename="$1-$(date +"%Y%m%d").tar.gz"
 
 docker run --rm \
   -v $1:/backup-volume:ro \
   busybox \
-  tar -zcvf /tmp/$filename -C /backup-volume .
+  sh -c "ls -A -1 /backup-volume | tar -cOzf - -C /backup-volume -T -" > /tmp/$filename
 
 s3cmd put --host=$S3_HOST --host-bucket=$S3_HOST_BUCKET /tmp/$filename s3://$S3_BUCKET/$(date +"%Y")/$(date +"%m")/$filename || exit 2
 
